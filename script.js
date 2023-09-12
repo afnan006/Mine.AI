9de58aefe7614f65960f902d1909f43acd686bba
darkbits018 <abhaygp18@gmail.com>
2023-09-12 12:36:56 +0530

    rapid api INTEGRATED

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const chatContainer = document.querySelector(".chat-container");
 
     // Function to handle the form submission
-    function handleSubmit(event) {
+    async function handleSubmit(event) {
         event.preventDefault(); // Prevent the form from submitting
 
         const question = userInput.value.trim(); // Get the user's question
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
             questionContainer.appendChild(engineerLogoImg);
             questionContainer.appendChild(questionDiv);
 
-            // Create a parent container for the logo and ans
+            // Create a parent container for the logo and answer
             const ansContainer = document.createElement("div");
             ansContainer.className = "ans-container";
 
@@ -38,12 +38,50 @@ document.addEventListener("DOMContentLoaded", function () {
             mineLogoImg.alt = "minegem";
             mineLogoImg.className = "logo-mine";
 
-            // Create a div element for the answer and set a default response
+            // Create a div element for the answer
             const answerDiv = document.createElement("div");
             answerDiv.className = "answer";
-            answerDiv.textContent = "Answer: I don't have an answer for that.";
 
-            // Append the engineer's logo and the user's question to the parent container
+            // Send the user's question to the API and get the response
+            //GET API FROM https://rapidapi.com/liuzhaolong765481/api/chatgpt-chatgpt3-5-chatgpt4
+            const url = 'https://chatgpt-chatgpt3-5-chatgpt4.p.rapidapi.com/v1/chat/completions';
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-RapidAPI-Key': 'API-KEY',
+                    'X-RapidAPI-Host': 'HOST-URL'
+                },
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: [
+                        {
+                            role: 'user',
+                            content: question
+                        }
+                    ],
+                    temperature: 0.8
+                })
+            };
+
+            try {
+                const response = await fetch(url, options);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch answer');
+                }
+                const data = await response.json();
+
+                if (data.choices && data.choices.length > 0) {
+                    const assistantResponse = data.choices[0].message.content || '';
+                    answerDiv.textContent = assistantResponse;
+                } else {
+                    answerDiv.textContent = 'Error: No response content found';
+                }
+            } catch (error) {
+                answerDiv.textContent = `Error: ${error.message}`;
+            }
+
+            // Append the engineer's logo and the answer to the parent container
             ansContainer.appendChild(mineLogoImg);
             ansContainer.appendChild(answerDiv);
 
@@ -55,7 +93,6 @@ document.addEventListener("DOMContentLoaded", function () {
             userInput.value = "";
         }
     }
-
 
     if (submitButton) {
         submitButton.addEventListener("click", handleSubmit);
